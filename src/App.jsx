c3e8eba2ff96b289390db6735d7c46cda1e3e97a
Johan Söderlund <johan.soderlund96@gmail.com>
2024-03-28 21:44:06 +0100

    router and icons install

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Board from "./components/Board";
-import Card from "./components/Card";
-import Column from "./components/Column";
+// import Card from "./components/Card";
+// import Column from "./components/Column";
+import Nav from "./components/Nav";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>KANBAN BOARD</h1>
+      <Nav />
       <Board />
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <h1 className="nav-headline">KANBAN BOARD</h1>
+    </div>
+  );
+};
+
+export default Nav;
