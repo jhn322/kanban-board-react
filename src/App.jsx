05269a277f8546437823976d335cd6eb72214b61
Johan Söderlund <johan.soderlund96@gmail.com>
2024-03-29 00:56:31 +0100

    column headline improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import Board from "./components/Board";
-// import Card from "./components/Card";
-// import Column from "./components/Column";
 import Nav from "./components/Nav";
-import "./App.css";
 
 function App() {
   return (
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -4,7 +4,9 @@ import Card from "./Card";
 const Column = ({ title, cards = [] }) => {
   return (
     <div className="column">
-      <h2>{title}</h2>
+      <div className="column-title">
+        <h2>{title}</h2>
+      </div>
       <div className="card-list">
         {cards.map((card, index) => (
           <Card key={index} text={card.text} />
